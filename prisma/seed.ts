3b8d4c9f1b69b60cb018d1f9953074ba606a746d
Nour Moussawi <nourmoussawi17@gmail.com>
2026-08-09 22:23:56 +0300

    edit url

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -127,7 +127,7 @@ async function main() {
       data: {
         businessName: 'GKarting',
         phone: '+961 00 000 000',
-        email: 'nrwork.mob@gmail.com',
+        email: 'nourmoussawi17@gmail.com',
         whatsapp: 'https://wa.me/96100000000',
         instagramUrl: 'https://instagram.com/nour_moussawi',
         facebookUrl: 'https://facebook.com/',
